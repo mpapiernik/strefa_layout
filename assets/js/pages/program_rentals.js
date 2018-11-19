46781b9fb317470735ef4a9ea1ca2d0041db6c7d
m.papiernik <m.kwidzinska@smartmedia.com.pl>
2018-11-19 22:20:26 +0100

    Aktualizacja makiety kalkulatora i sprzetu i wyposazenia

--- assets/js/pages/program_rentals.js
+++ assets/js/pages/program_rentals.js
@@ -1,15 +1,28 @@
 jQuery(document).ready(function () {
     App.init();
+
+
 });
 
 
-function refreshModalAmounts(rentalAmountOutput, installmentAmountOutput) {
+function refreshModalAmounts(installmentAmountOutput, rentalAmountOutput, months) {
     var rentalAmountModal = document.getElementById("rent_amount_input_modal");
     var rentalMonthsModal = document.getElementById("rent_months_input_modal");
+    var rentalInstallmentModal = document.getElementById("rent_installment_input_modal");
     rentalAmountModal.value  = rentalAmountOutput;
-    rentalMonthsModal.value = installmentAmountOutput;
-
+    rentalMonthsModal.value = months;
+    rentalInstallmentModal.value = installmentAmountOutput;
 };
+
+function appendToIrsGridText(rent_span_name, appenddedText) {
+    var sliderAmountSpan = document.getElementById(rent_span_name);
+    var spans = sliderAmountSpan.getElementsByClassName("irs-grid-text")
+    for (let value of spans) {
+        value.append(appenddedText);
+    }
+};
+
+
 
 $(function () {
    var interest = 105;
@@ -21,46 +34,42 @@ $(function () {
     var installmentAmountOutput = document.getElementById("installment_amount");
     var rentalAmountOutput = document.getElementById("rental_amount");
 
-
     installmentAmountOutput.innerHTML = slider.value; // Display the default slider value
 
 
     var months = 12;
-    var rentalAmmount = 1000;
-    rentalAmountOutput.value = (Math.round((rentalAmmount * interest)) / 100).toFixed(2);
-    installmentAmountOutput.value = (Math.round((rentalAmmount * interest)) /months / 100).toFixed(2);
-    refreshModalAmounts();
+    var rentalAmount = 100000;
+    rentalAmountOutput.value = (Math.round((rentalAmount * interest)) / 100).toFixed(2);
+    installmentAmountOutput.value = (Math.round((rentalAmount * interest)) /months / 100).toFixed(2);
 
     sliderMonths.oninput = function() {
         months = this.value;
-        rentalAmountOutput.value = (Math.round((rentalAmmount * interest)) / 100).toFixed(2);
-        installmentAmountOutput.value = (Math.round((rentalAmmount * interest)) /months / 100).toFixed(2);
-        refreshModalAmounts(installmentAmountOutput.value, months);
+        rentalAmountOutput.value = (Math.round((rentalAmount * interest)) / 100).toFixed(2);
+        installmentAmountOutput.value = (Math.round((rentalAmount * interest)) /months / 100).toFixed(2);
+        refreshModalAmounts(installmentAmountOutput.value, rentalAmount, months);
     }
 
     sliderMonthsInput.oninput = function() {
         months = sliderMonths.value;
-        rentalAmountOutput.value = (Math.round((rentalAmmount * interest)) / 100).toFixed(2);
-        installmentAmountOutput.value = (Math.round((rentalAmmount * interest)) /months / 100).toFixed(2);
-        refreshModalAmounts(installmentAmountOutput.value, months);
+        rentalAmountOutput.value = (Math.round((rentalAmount * interest)) / 100).toFixed(2);
+        installmentAmountOutput.value = (Math.round((rentalAmount * interest)) /months / 100).toFixed(2);
+        refreshModalAmounts(installmentAmountOutput.value, rentalAmount, months);
     }
 
     // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function() {
-        rentalAmmount = this.value;
-        rentalAmountOutput.value = (Math.round((rentalAmmount * interest)) / 100).toFixed(2);
-        installmentAmountOutput.value = (Math.round((rentalAmmount * interest)) /months / 100).toFixed(2);
-        refreshModalAmounts(installmentAmountOutput.value, months);
+        rentalAmount = this.value;
+        rentalAmountOutput.value = (Math.round((rentalAmount * interest)) / 100).toFixed(2);
+        installmentAmountOutput.value = (Math.round((rentalAmount * interest)) /months / 100).toFixed(2);
+        refreshModalAmounts(installmentAmountOutput.value,rentalAmount, months);
     }
 
     sliderInput.oninput = function() {
-        rentalAmmount = this.value;
-        rentalAmountOutput.value = (Math.round((rentalAmmount * interest)) / 100).toFixed(2);
-        installmentAmountOutput.value = (Math.round((rentalAmmount * interest)) /months / 100).toFixed(2);
-        refreshModalAmounts(installmentAmountOutput.value, months);
+        rentalAmount = this.value;
+        rentalAmountOutput.value = (Math.round((rentalAmount * interest)) / 100).toFixed(2);
+        installmentAmountOutput.value = (Math.round((rentalAmount * interest)) /months / 100).toFixed(2);
+        refreshModalAmounts(installmentAmountOutput.value, rentalAmount, months);
     }
-
-
 
 });
 
